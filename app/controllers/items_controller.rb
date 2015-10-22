3dec3fb147e852d0631d1ec075f9d90501937d41
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-10-22 10:34:41 -0700

    Make admin page cooler.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
   def new
+    @location = Location.find(params[:location_id])
     @item = Item.new
   end
+
+  def create
+    @location = Location.find(params[:location_id])
+    @item = @location.items.new(item_params)
+    if @item.save
+      redirect_to admin_index_path
+    else
+      flash[:notice] = "No sirree bob"
+      redirect_to admin_index_path
+    end
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description)
+    end
 end
